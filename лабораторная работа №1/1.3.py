c4c887b6c44795d6e54c060c72f63f776f30c67e
umarnuremar <nuremar100@gmail.com>
2024-10-20 15:22:20 +0300

    Add files via upload

---
+++ лабораторная работа №1/1.3.py
@@ -0,0 +1,9 @@
+3
+players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+# индекс середины
+midindex = len(players) // 2
+team_one = players[:midindex]
+team_two = players[midindex:]
+print(team_one)
+print(team_two)
+
