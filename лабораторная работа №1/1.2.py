92858ed42d4c384c728a1c7d708580fd45b9b158
umarnuremar <nuremar100@gmail.com>
2024-11-22 01:12:31 +0300

    Update 1.2.py

--- лабораторная работа №1/1.2.py
+++ лабораторная работа №1/1.2.py
@@ -1,14 +1,15 @@
-2
-#описание книги
-pages = 100           #колво страниц
-linesperpage = 50   # колво строк на странице
-symbperline = 25   #колво символов в строке
-bytespersymb = 4    #байты для одного символа
-#одна книга в байтах
-booksize_bytes = pages * linesperpage * symbperline * bytespersymb
-disksize_mb = 1.44
-disksize_bytes = disksize_mb * 1024 * 1024  #в байты переводим
-#колво книг на дискете
-numbooks = int(disksize_bytes // booksize_bytes)
-print("Количество книг, помещающихся на дискету:", numbooks)
+# Параметры книги
+pages = 100           # количество страниц
+lines_per_page = 50   # число строк на странице
+chars_per_line = 25   # количество символов в строке
+bytes_per_char = 4    # байты для хранения одного символа
 
+# объем одной книги в байтах
+book_size_bytes = pages * lines_per_page * chars_per_line * bytes_per_char
+
+disk_size_mb = 1.44
+disk_size_bytes = disk_size_mb * 1024 * 1024  # в байты
+
+# количество книг, которые можно поместить на дискету
+number_of_books = int(disk_size_bytes // book_size_bytes)
+print("Количество книг, помещающихся на дискету:", number_of_books)
