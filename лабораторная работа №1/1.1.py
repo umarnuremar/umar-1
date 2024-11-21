2b67b4ef52950674cf2559cda49c754dd479cc13
umarnuremar <nuremar100@gmail.com>
2024-11-22 01:02:58 +0300

    Update 1.1.py

--- лабораторная работа №1/1.1.py
+++ лабораторная работа №1/1.1.py
@@ -1,10 +1,10 @@
-nomer = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-missing_index = nomer.index(None)  #решили что пропущенный элемент обозначен как None
-#считаем сумму и количество элементов без пропущенного
-total_sum = sum(num for num in nomer if num is not None)
-count = len(nomer)  #количество элементов включая пропуск
-#среднее арифметическое
-average = total_sum / count  #убираем пропуск из суммы
-#замена пропущенного элемента средним арифметическим
-nomer[missing_index] = average
-print("Измененный список:", nomer)
+numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+missing_index = numbers.index(None)  # Предполагаем, что пропущенный элемент обозначен как None
+# Вычислим сумму и количество элементов, исключая пропущенный
+total_sum = sum(num for num in numbers if num is not None)
+count = len(numbers)  # Общее количество элементов, включая пропуск
+# Рассчитаем среднее арифметическое
+average = total_sum / count  # Исключаем пропуск из суммы
+# Заменяем пропущенный элемент средним арифметическим
+numbers[missing_index] = average
+print("Измененный список:", numbers)
